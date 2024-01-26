174189fbb9093d75143d2d8cc2917316bcfd70bb
Felipe Santos <felipek2267@gmail.com>
2024-01-25 22:27:13 -0300

    ajustando tela

--- index.js
+++ index.js
@@ -4,37 +4,72 @@ let palpites = [];
 formulario.onsubmit = () => {
     event.preventDefault();
 
-    if(palpites.length < 9 ) {
-        if(palpite.value == numeroRandomico) {
-            again.classList.add('reset');
-            winner.classList.add('sucess');
+    if (palpites.length < 9) {
+
+        if (palpite.value == numeroRandomico) {
+
+            again.classList.add("reset");
+            winner.classList.add("sucess");
+            winner.innerHTML = `Parabens Voce acertou!! O numero é: ${numeroRandomico}`;
             palpite.remove();
             img_bia.src = "img/bia_feliz.png";
             return;
         }
-    
+        if(palpite.value < numeroRandomico) {
+            alert("É maior");
+        }else {
+            alert("É menor");
+        }
+
         if(palpites.includes(palpite.value)){
-            alert('Este palpite ja foi dado');
+            alert("Esse palpite ja foi dado!!")
             formulario.reset();
             return;
         }
 
-        if(palpite.value < numeroRandomico) {
-            alert('É maior')
-        }else{
-            alert('É menor')
-        }
-    
         palpites.push(palpite.value);
-        palpitesText.innerHTML = palpites.join(' - ');
+        palpitesText.innerHTML = palpites.join(" - ");
         formulario.reset();
-
     }else {
-        again.classList.add('reset');
+        again.classList.add("reset");
+        winner.classList.add("sucess");
+        winner.innerHTML = `Ha Não !! O numero é: ${numeroRandomico}`;
         img_bia.src = "img/bia_triste.png";
     }
+
 }
 
 again.onclick = () => {
     window.location.reload();
 }
+
+
+// if(palpites.length < 9 ) {
+//     if(palpite.value == numeroRandomico) {
+//         again.classList.add('reset');
+//         winner.classList.add('sucess');
+//         palpite.remove();
+//         img_bia.src = "img/bia_feliz.png";
+//         return;
+//     }
+
+//     if(palpites.includes(palpite.value)){
+//         alert('Este palpite ja foi dado');
+//         formulario.reset();
+//         return;
+//     }
+
+//     if(palpite.value < numeroRandomico) {
+//         alert('É maior')
+//     }else{
+//         alert('É menor')
+//     }
+
+//     palpites.push(palpite.value);
+//     palpitesText.innerHTML = palpites.join(' - ');
+//     formulario.reset();
+
+// }else {
+//     again.classList.add('reset');
+//     img_bia.src = "img/bia_triste.png";
+// }
